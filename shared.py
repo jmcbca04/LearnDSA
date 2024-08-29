6273ed062133d49c12f27dd2428f3753c140b463
Jay McCauley <jay.a.mccauley@gmail.com>
2024-08-28 20:26:02 -0400

    removed debug & logging statements

--- shared.py
+++ shared.py
@@ -10,12 +10,9 @@ logger = logging.getLogger(__name__)
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 ALGORITHM = "HS256"
 
-logger.debug(f"JWT_SECRET_KEY: {JWT_SECRET_KEY}")
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
-    logger.debug(f"Received token: {token}")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -26,7 +23,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     try:
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
-        logger.debug(f"Decoded payload: {payload}")
         email: str = payload.get("email")
         if email is None:
             logger.error("Email claim is missing in the token")
@@ -40,11 +36,9 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
 async def get_optional_user(token: str = Depends(oauth2_scheme)):
-    logger.debug(f"Received token: {token[:10] + '...' if token else None}")
     if token:
         try:
             payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
-            logger.debug(f"Decoded payload: {payload}")
             email: str = payload.get("email")
             if email is None:
                 logger.error("Email claim is missing in the token")
